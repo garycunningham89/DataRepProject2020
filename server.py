d49914fa0087975d0d4f28e3294dcb3ed88095b2
garycunningham89 <47434962+garycunningham89@users.noreply.github.com>
2021-01-04 22:52:01 +0000

    Update server.py

--- server.py
+++ server.py
@@ -99,5 +99,4 @@ def delete(userid):
     return jsonify({"done": True})
 # curl -X DELETE http://127.0.0.1:5000/training/1001
 if __name__ == "__main__":
-   db.create_all()
     app.run(debug=True)
